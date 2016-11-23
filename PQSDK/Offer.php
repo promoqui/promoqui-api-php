0605745e9f0677da65617e43097a8464e0b9ea88
Florin Lungu <me@florinlungu.it>
2016-11-23 14:36:16 +0100

    Updated Offers wrapper

--- PQSDK/Offer.php
+++ PQSDK/Offer.php
@@ -3,7 +3,7 @@
 namespace PQSDK;
 
 class Offer {
-  public $title, $description, $price, $original_price, $discount, $start_date, $end_date, $image, $store_ids;
+  public $title, $description, $price, $original_price, $discount, $start_date, $end_date, $image, $store_ids, $national, $partner_link, $go_to_partner_link, $btn_other_offers_visible, $btn_partner_link_text, $btn_partner_link_visible, $btn_print_visible, $btn_stores_visible, $btn_online_offers_visible;
 
 
   public function __construct($params = array()){
@@ -13,10 +13,59 @@ class Offer {
       }
     }
     if(empty($this->store_ids)){
-    	$this->$store_ids = [];
+      $this->$store_ids = [];
     }
   }
 
+  public function national(){
+    return empty($this->national) ? False : $this->national;
+  }
+
+  public function store_ids(){
+    return empty($this->store_ids) ? array() : $this->store_ids;
+  }
+
+  public function btn_other_offers_visible() {
+    if (empty($this->btn_other_offers_visible)) {
+      return $this->btn_other_offers_visible = true;
+    }
+    return $this->btn_other_offers_visible;
+  }
+
+  public function btn_partner_link_visible() {
+    if (empty($this->btn_partner_link_visible)){
+      return $this->btn_partner_link_visible = true;
+    }
+    return $this->btn_partner_link_visible;
+  }
+
+  public function btn_print_visible() {
+    if (empty($this->btn_print_visible)) {
+      return $this->btn_print_visible = true;
+    }
+    return $this->btn_print_visible;
+  }
+
+  public function btn_stores_visible() {
+    if (empty($this->btn_stores_visible)) {
+      return $this->btn_stores_visible = true;
+    }
+    return $this->btn_stores_visible;
+  }
+
+  public function btn_online_offers_visible() {
+    if (empty($this->btn_online_offers_visible)) {
+      return $this->btn_online_offers_visible = true;
+    }
+    return $this->btn_online_offers_visible;
+  }
+
+  public function go_to_partner_link() {
+    if (empty($this->go_to_partner_link)) {
+      return $this->go_to_partner_link = true;
+    }
+    return $this->go_to_partner_link;
+  }
 
   public function token() {
     return Token::accessToken();
